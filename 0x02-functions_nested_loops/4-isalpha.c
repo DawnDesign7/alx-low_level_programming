c84c2dc2cc124a80217ecb1f3dcfa439d574ad34
DawnDesign7 <dawnmoraa@gmail.com>
2022-03-15 13:25:16 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabetic characters
+ *
+ * Description: checks for alpha
+ *
+ * return: 1 - alphabetic character 0 - otherwise
+ *
+ * @c: input
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
